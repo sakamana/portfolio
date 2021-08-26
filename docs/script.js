dab11752df8fccaccba3fd9688825020a971f7ab
sakamana <sakuma-nao@ed.tmu.ac.jp>
2021-08-26 16:12:00 +0900

    横方向に増殖
    二重でfor囲めば楽しそう

--- docs/script.js
+++ docs/script.js
@@ -3,15 +3,20 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.querySelector("#threeJS").appendChild( renderer.domElement );
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+
+for ( var i = 1 ; i <= 5 ; i++ ){
+  const geometry = new THREE.BoxGeometry();
+  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  const cube = new THREE.Mesh( geometry, material);
+  scene.add( cube );
+  cube.position.set(i-10, i-4, 0);
+
+  const animate = function () {
+    cube.rotation.x = cube.rotation.x + 0.01;
+    cube.rotation.y = cube.rotation.y + 0.01;
+    renderer.render( scene, camera );
+    requestAnimationFrame( animate );
+  };
+  animate();
+}
 camera.position.z = 5;
-const animate = function () {
-  cube.rotation.x = cube.rotation.x + 0.01;
-  cube.rotation.y = cube.rotation.y + 0.01;
-  renderer.render( scene, camera );
-  requestAnimationFrame( animate );
-};
-animate();
