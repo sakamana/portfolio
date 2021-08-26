81c060afdc8c44ffe9324372af5f3a89644debca
sakamana <sakuma-nao@ed.tmu.ac.jp>
2021-08-26 16:24:51 +0900

    縦横に１５こずつ

--- docs/script.js
+++ docs/script.js
@@ -4,19 +4,21 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.querySelector("#threeJS").appendChild( renderer.domElement );
 
-for ( var i = 1 ; i <= 5 ; i++ ){
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  const cube = new THREE.Mesh( geometry, material);
-  scene.add( cube );
-  cube.position.set(i-10, i-4, 0);
+for ( var j = 1 ; j <= 15 ; j++ ){
+  for ( var i = 1 ; i <= 15 ; i++ ){
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial( { color: 0x808080 } );
+    const cube = new THREE.Mesh( geometry, material);
+    scene.add( cube );
+    cube.position.set(2.5*i-20, 2.5*j-6, 0);
 
-  const animate = function () {
-    cube.rotation.x = cube.rotation.x + 0.01;
-    cube.rotation.y = cube.rotation.y + 0.01;
-    renderer.render( scene, camera );
-    requestAnimationFrame( animate );
-  };
-  animate();
+    const animate = function () {
+      cube.rotation.x = cube.rotation.x + 0.01;
+      cube.rotation.y = cube.rotation.y + 0.01;
+      renderer.render( scene, camera );
+      requestAnimationFrame( animate );
+    };
+    animate();
+  }
 }
 camera.position.z = 5;
